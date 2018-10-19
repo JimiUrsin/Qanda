ad47cac13e158908de63f83dab6ca40260da358c
JimiUrsin <jimi.ursin@gmail.com>
2018-10-19 15:35:26 +0300

    Maven plugin for Heroku to make sure it downloads all dependencies

--- src/main/java/com/qanda/Database.java
+++ src/main/java/com/qanda/Database.java
@@ -8,12 +8,12 @@ public class Database {
 
     private String databaseAddress;
 
-    public Database(String databaseAddress) throws ClassNotFoundException {
-        this.databaseAddress = System.getenv("JDBC_DATABASE_URL");
+    public Database() throws ClassNotFoundException {
+        this.databaseAddress = System.getenv("JDBC_DATABASE_URL"); // Korvaa hardcodettu databaseAddressillä jos pyöritetään lokaalisti
     }
 
     public Connection getConnection() throws SQLException {
-        /*
+        /* Lokaalia pyöritystä varten
         Properties props = new Properties();
         props.setProperty("user", "postgres");
         props.setProperty("password", "root");
--- src/main/java/com/qanda/Qanda.java
+++ src/main/java/com/qanda/Qanda.java
@@ -13,9 +13,14 @@ public class Qanda {
     public static void main(String[] args) {
 
 
+        try {
+            Spark.port(Integer.valueOf(System.getenv("PORT"))); // needed for Heroku
+        } catch (Exception e) {
+            System.err.println("PORT-ympäristömuuttujaa ei löytynyt, käytetään defaultia.");
+        }
         Database database = null;
         try {
-            database = new Database("jdbc:postgresql://localhost/main");
+            database = new Database();
         } catch (ClassNotFoundException cnfe) {
             cnfe.printStackTrace();
             System.exit(0);
